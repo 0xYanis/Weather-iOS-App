68eef5bba7099304cc7a5407573e6bdf7976c88a
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-03-14 12:38:56 +0300

    Create ViewController

---
+++ WeatherApp/MainModule/View/MainViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MainViewController.swift
+//  WeatherApp
+//
+//  Created by Yan Rybkin on 14.03.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+	
+	//MARK: VC lifecycle
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		initialize()
+	}
+}
+
+extension MainViewController: MainViewProtocol {
+	func setLocation(location: String) {
+		//
+	}
+	
+	func setAnotherForecast(dayNumber: Int) {
+		//
+	}
+	
+	
+}
+
+// MARK: Private methods
+private extension MainViewController {
+	func initialize() {
+		
+	}
+}
