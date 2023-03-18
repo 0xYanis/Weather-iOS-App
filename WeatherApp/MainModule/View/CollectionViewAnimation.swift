37288b1874b30e32a8e03f0cfa32af75c1a3f109
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-03-18 14:15:29 +0300

    Add animation to reloadData method

---
+++ WeatherApp/MainModule/View/CollectionViewAnimation.swift
@@ -0,0 +1,22 @@
+//
+//  CollectionViewAnimation.swift
+//  WeatherApp
+//
+//  Created by Yan Rybkin on 18.03.2023.
+//
+
+import UIKit
+
+class CollectionViewAnimation {
+	
+	static func animateReloadData(collectionView: UICollectionView) {
+		UIView.animate(withDuration: 0.0, animations: {
+			collectionView.alpha = 0
+		}, completion: { _ in
+			collectionView.reloadData()
+			UIView.animate(withDuration: 0.3) {
+				collectionView.alpha = 1
+			}
+		})
+	}
+}
