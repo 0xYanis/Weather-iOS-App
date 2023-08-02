5b4848700f675803c46d515c08f90cdcfe6ec5b7
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-08-02 22:32:42 +0300

    Refactoring location service

--- WeatherApp/ServiceLayer/LocationService.swift
+++ WeatherApp/ServiceLayer/LocationService.swift
@@ -9,23 +9,28 @@ import Foundation
 import CoreLocation
 
 protocol LocationServiceProtocol: AnyObject {
-	func getUserLocation(from address: String, completion: @escaping ([Double]?) -> Void)
+    typealias completionBlock = ([Double]?) -> Void
+	func getUserLocation(from address: String, completion: @escaping completionBlock)
 }
 
-class LocationService: NSObject {
+class LocationService {
 	let geocoder = CLGeocoder()
 }
 
 extension LocationService: LocationServiceProtocol {
-	func getUserLocation(from address: String, completion: @escaping ([Double]?) -> Void) {
+    
+    typealias completionBlock = ([Double]?) -> Void
+    
+	func getUserLocation(from address: String, completion: @escaping completionBlock) {
 		geocoder.geocodeAddressString(address) { placemarks, error in
 			guard let placemark = placemarks?.first, error == nil else {
-				completion(nil)
-				return
+                completion(nil); return
 			}
 			let latitude = placemark.location?.coordinate.latitude ?? 0.0
 			let longitude = placemark.location?.coordinate.longitude ?? 0.0
+            
 			completion([latitude, longitude])
 		}
 	}
+    
 }
